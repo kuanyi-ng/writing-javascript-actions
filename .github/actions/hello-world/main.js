ce498e61354f0d82012167a9c7db8e2549d0f874
KY-Ng <raionkurane@gmail.com>
2021-01-30 14:05:10 +0900

    handle input in main.js;

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import { getInput } from "@actions/core";
+
+const firstGreeting = getInput('first-greeting');
+const secondGreeting = getInput('second-greeting');
+const thirdGreeting = getInput('third-greeting');
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+if (thirdGreeting) {
+  console.log(`Hello ${thirdGreeting}`);
+}
